72cf08cb009bc7b12a45810724b94af90c052d72
licensede <pablped@gmail.com>
2021-01-28 19:33:16 +0100

    reversed string solved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,43 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // -------------------------
+    //solution 1
+    // -------------------------
+
+    // const arr = str.split('');
+    // arr.reverse();
+    // return arr.join('');
+
+    // --------------------------
+    //solution 2 which is the same as 1, but better formatted
+    // -------------------------
+
+    // return str
+    //     .split('')
+    //     .reverse()
+    //     .join('')
+
+    // -------------------------
+    //solution 3
+    // -------------------------
+    
+    // let reversed = "";
+
+    // for (let character of str) {
+    //     reversed = character + reversed;
+    // }
+
+    // return reversed;
+
+    // -------------------------
+    //solution 4
+    // -------------------------
+
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed;
+    }, '');
+}
 
 module.exports = reverse;
